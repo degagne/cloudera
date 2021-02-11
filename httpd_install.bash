241679e87d51e88d02970ad78665b49c1fc6cf74
degagne <deric.degagne@gmail.com>
2021-02-11 11:08:54 -0500

    Update httpd_install.bash

--- httpd_install.bash
+++ httpd_install.bash
@@ -5,5 +5,5 @@ systemctl start httpd
 systemctl status httpd
 
 mkdir -p /var/www/html/cloudera-repos
-wget --recursive --no-parent --no-host-directories https://archive.cloudera.com/p/cdh7/7.1.4/parcels/ -P /var/www/html/cloudera-repos
+wget --recursive --no-parent --no-host-directories https://archive.cloudera.com/cdh7/7.1.4/parcels/ -P /var/www/html/cloudera-repos
 chmod -R ugo+rX /var/www/html/cloudera-repos/cdh7
