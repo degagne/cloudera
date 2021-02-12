6f06d6434df511a7e0940c3d992047d03b592d9c
ddegagne <ddegagne@blackberry.com>
2021-02-11 21:55:29 -0500

    updates

--- httpd_install.bash
+++ httpd_install.bash
@@ -4,6 +4,31 @@ systemctl enable httpd
 systemctl start httpd
 systemctl status httpd
 
+cat > /etc/yum.repos.d/cloudera-manager.repo << EOF
+[cloudera-manager]
+name=Cloudera Manager 7.1.4
+baseurl=https://archive.cloudera.com/cm7/7.1.4/redhat7/yum/
+gpgkey=https://archive.cloudera.com/cm7/7.1.4/redhat7/yum/RPM-GPG-KEY-cloudera
+gpgcheck=1
+enabled=1
+autorefresh=0
+
+[postgresql10]
+name=Postgresql 10
+baseurl=https://archive.cloudera.com/postgresql10/redhat7/
+gpgkey=https://archive.cloudera.com/postgresql10/redhat7/RPM-GPG-KEY-PGDG-10
+enabled=1
+gpgcheck=1
+EOF
+
+mkdir -p /var/www/html/cm7/7.1.4/redhat7/yum
+reposync -r cloudera-manager -p /var/www/html
+mv /var/www/html/cloudera-manager/RPMS /var/www/html/cm7/7.1.4/redhat7/yum
+createrepo /var/www/html/cm7/7.1.4/redhat7/yum
+wget https://archive.cloudera.com/cm7/7.1.4/redhat7/yum/RPM-GPG-KEY-cloudera -P /var/www/html/cm7/7.1.4/redhat7/yum
+
+sed -i 's/https:\/\/archive\.cloudera\.com/http:\/\/bigdataserver-1/g' /etc/yum.repos.d/cloudera-manager.repo
+
 mkdir -p /var/www/html/cloudera-repos
 wget --recursive --no-parent --no-host-directories https://archive.cloudera.com/cdh7/7.1.4/parcels/ -P /var/www/html/cloudera-repos
 chmod -R ugo+rX /var/www/html/cloudera-repos/cdh7
